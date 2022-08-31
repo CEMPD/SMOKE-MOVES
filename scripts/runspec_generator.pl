2b7cfca68d6e05a7bb1f964b1d837586659a9381
Catherine Seppanen <cas@whpress.com>
2022-08-31 17:24:33 -0400

    Remove MTBE (#2)

--- scripts/runspec_generator.pl
+++ scripts/runspec_generator.pl
@@ -207,7 +207,7 @@ my ( @pollsOutList );
 #  I don't know how often these pollutant subset options are used in practice.
 #  March 2016 - reorganized options and made sure all prerequisites are included for each set;
 #    see https://github.com/CEMPD/SMOKE-MOVES/wiki/Runspec-generator-pollutant-options
-@pollsByOptionOZONE = (1,2,3,5,20,21,22,23,24,25,26,27,32,33,34,40,41,42,43,44,45,46,79,80,86,87,185,1000,1500);
+@pollsByOptionOZONE = (1,2,3,5,20,21,23,24,25,26,27,32,33,34,40,41,42,43,44,45,46,79,80,86,87,185,1000,1500);
 @pollsByOptionPM = (1,30,31,35,36,51,52,53,54,55,56,57,58,59,66,91,100,106,107,110,111,112,115,116,117,118,119,121,122);
 @pollsByOptionTOXICS = (1,68,69,70,71,72,73,74,75,76,77,78,79,81,82,83,84,87,111,115,118,119,
 130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,168,169,170,171,172,173,174,175,176,177,178,181,182,183,184);
